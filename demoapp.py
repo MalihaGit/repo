4603974d7e7f1a3e5b65fb1d011ee79487b6fa8b
Maliha Ganesh <107932008+MalihaGit@users.noreply.github.com>
2022-07-02 11:59:17 +0000

    demoapp commited

--- demoapp.py
+++ demoapp.py
@@ -1,1 +1,7 @@
-
+from flask import Flask
+app=Flask(__name__)
+@app.route("/")
+def greetings():
+  return "Hello this is Maliha & i love stranger things"
+if __name__=="__main__":
+  app.run("0.0.0.0")
